7fb40f961234f6ee0aea13d727bd74a91bf1dadc
EtienneV <etiennev69@hotmail.fr>
2014-08-09 16:25:57 +0200

    Ajout LED

--- Codes source de départ/Exemple_stepper.ino
+++ Codes source de départ/Exemple_stepper.ino
@@ -10,6 +10,8 @@ Stepper myStepper(stepsPerRevolution, 2,3,4,5);  // Initialisation du contrôle 
 
 void setup()  {
   //Serial.begin(9600);
+
+  pinMode(13, OUTPUT);
 }
 
 void loop()
@@ -17,4 +19,11 @@ void loop()
   myStepper.step(1); // Fait avancer d'un pas   
 
   delay(500); // On attend une demi seconde
+
+  digitalWrite(13, HIGH);
+
+  delay(500); // On attend une demi seconde
+
+  digitalWrite(13, LOW);  
+
 }
