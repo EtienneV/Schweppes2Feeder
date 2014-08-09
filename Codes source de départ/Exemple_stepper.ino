df596924f6646c47d960558090fc88debfffe23c
EtienneV <etiennev69@hotmail.fr>
2014-08-09 13:04:18 +0200

    Réduction du code du stepper au strict minimum

--- Codes source de départ/Exemple_stepper.ino
+++ Codes source de départ/Exemple_stepper.ino
@@ -1,118 +1,20 @@
-//#include <Time.h>  
-//#include <Wire.h>  
-//#include <DS1307RTC.h>  
+/*
+* Fait tourner le moteur pas a pas
+*
+*/
+
 #include <Stepper.h>
 
-const int ledPin = 7;
-const int interPin = 6;
-const int baseDeTemps = 0;
-boolean etatBaseDeTemps = 0;
-
-boolean buffInter = 0;
-boolean etatLed = 0;
-boolean attente = 0;
-
-long timerSeconde = millis();
-long timerDemi = millis();
-
-const int stepsPerRevolution = 48; 
-Stepper myStepper(stepsPerRevolution, 2,3,4,5);  
+const int stepsPerRevolution = 48; // Nombre de pas par tour
+Stepper myStepper(stepsPerRevolution, 2,3,4,5);  // Initialisation du contrôle du moteur
 
 void setup()  {
   //Serial.begin(9600);
-
-  delay(2000);
-
-  //setSyncProvider(RTC.get); 
-
-  delay(1000);
-
-  pinMode(ledPin, OUTPUT);
-  pinMode(interPin, INPUT);
-
 }
 
 void loop()
 {
-  boolean etatInter = digitalRead(interPin);
-  if(etatInter){
-    buffInter = 1;
+  myStepper.step(1); // Fait avancer d'un pas   
 
-  }
-  if((!etatInter) && (buffInter)){
-    buffInter = 0;
-    chronometre();
-  }
-
-  if(digitalRead(baseDeTemps) != etatBaseDeTemps){   
-    if(digitalRead(baseDeTemps)){
-      myStepper.step(1);
-
-      digitalWrite(ledPin, HIGH); 
-
-      timerSeconde = millis();
-    }     
-
-    etatBaseDeTemps = !etatBaseDeTemps;
-    etatLed = !etatLed;
-    digitalWrite(ledPin, etatLed); 
-    timerDemi = millis();
-  }
+  delay(500); // On attend une demi seconde
 }
-
-int chronometre()
-{
-  int continuer = 1;
-
-  while(continuer){
-    if(digitalRead(baseDeTemps) != etatBaseDeTemps){   
-      if(digitalRead(baseDeTemps)){
-        myStepper.step(1);
-
-        timerSeconde = millis();
-      }     
-
-      etatBaseDeTemps = !etatBaseDeTemps;
-      etatLed = !etatLed;
-      digitalWrite(ledPin, etatLed); 
-      timerDemi = millis();
-    }
-
-    boolean etatInter = digitalRead(interPin);
-    if(etatInter){
-      buffInter = 1;
-    }
-    if((!etatInter) && (buffInter)){
-
-      buffInter = 0;
-      attente = 1;
-
-      while(attente){
-        boolean etatInter = digitalRead(interPin);
-        if(etatInter){
-          buffInter = 1;
-
-        }
-        if((!etatInter) && (buffInter)){
-          buffInter = 0;
-          attente = 0;
-          continuer = 0;
-        }
-        if(digitalRead(baseDeTemps) != etatBaseDeTemps){   
-          if(digitalRead(baseDeTemps)){
-            myStepper.step(1);
-            timerSeconde = millis();
-          }     
-
-          etatBaseDeTemps = !etatBaseDeTemps;
-          etatLed = !etatLed;
-          digitalWrite(ledPin, etatLed); 
-          timerDemi = millis();
-        }
-      }
-
-
-    }
-  }
-}
-
