f03705ac77fd1c4be0a2106458ec6f620641ebb4
EtienneV <etiennev69@hotmail.fr>
2014-08-09 19:46:38 +0200

    Réorganisation des sources d'exemple

--- Codes source de départ/Accel Stepper/Test_AccelStepper.ino
+++ Codes source de départ/Accel Stepper/Test_AccelStepper.ino
@@ -13,18 +13,17 @@ AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4
 
 void setup()
 {  
+	stepper.setMaxSpeed(100);
+	stepper.setAcceleration(10);
 }
 
 void loop()
 {
     if (stepper.distanceToGo() == 0)
     {
-	// Random change to speed, position and acceleration
-	// Make sure we dont get 0 speed or accelerations
-	delay(1000);
-	stepper.moveTo(rand() % 200);
-	stepper.setMaxSpeed((rand() % 200) + 1);
-	stepper.setAcceleration((rand() % 200) + 1);
+		delay(1000);
+		stepper.moveTo(24);
     }
+    
     stepper.run();
 }
